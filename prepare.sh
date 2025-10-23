e19e40a7549540e657a0a0db6739e05ad0e1f3fd
Junho Jeong <junho@falinux.com>
2025-10-23 18:17:54 +0900

    Run add-apt-repository in non-interactive mode.

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt update
 sudo apt install -y software-properties-common curl
-sudo add-apt-repository universe
+sudo add-apt-repository -y universe
 
 ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
